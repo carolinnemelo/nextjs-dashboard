3c67022a5a4deacb09a485a51d05914daa58975c
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-09 11:46:13 +0100

    recap

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,20 @@
-import { Card } from "@/app/ui/dashboard/cards";
-import RevenueChart from "../ui/dashboard/revenue-chart";
-import LatestInvoices from "../ui/dashboard/latest-invoices";
+// import { Card } from "@/app/ui/dashboard/cards";
+// import RevenueChart from "../ui/dashboard/revenue-chart";
+// import LatestInvoices from "../ui/dashboard/latest-invoices";
 import { lusitana } from "../ui/fonts";
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data";
+// import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data";
 
 export default async function Page() {
-    const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
-    const { totalPaidInvoices, numberOfCustomers, numberOfInvoices, totalPendingInvoices } = await fetchCardData();
+    // const revenue = await fetchRevenue();
+    // const latestInvoices = await fetchLatestInvoices();
+    // const { totalPaidInvoices, numberOfCustomers, numberOfInvoices, totalPendingInvoices } = await fetchCardData();
 
 	return (
 		<main>
 			<h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
 				Dashboard
 			</h1>
-			<div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+			{/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
 				<Card title= "Collected" value={totalPaidInvoices} type="collected" />
 				<Card 
                 title="Pending" 
@@ -27,10 +27,10 @@ export default async function Page() {
                 value={numberOfCustomers}
                 type="customers"
                 />
-			</div>
+			</div> */}
 			<div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-				<RevenueChart revenue={revenue}  />
-				<LatestInvoices latestInvoices={latestInvoices} />
+				{/* <RevenueChart revenue={revenue}  /> */}
+				{/* <LatestInvoices latestInvoices={latestInvoices} /> */}
 			</div>
 		</main>
 	);
