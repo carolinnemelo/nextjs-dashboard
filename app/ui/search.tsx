c8b2959bf25a2f0df599cd8cdf88763af486859a
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-09 15:14:17 +0100

    add and use debounce library

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useDebouncedCallback } from "use-debounce";
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams } from "next/navigation";
 import { usePathname, useRouter } from "next/navigation";
@@ -8,15 +9,16 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathName = usePathname();
   const { replace } = useRouter();
-
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
     if (!term) {
       params.delete("query");
     }
     params.set("query", term);
-    replace(`${pathName}?${params.toString()}`)
-  }
+    replace(`${pathName}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
